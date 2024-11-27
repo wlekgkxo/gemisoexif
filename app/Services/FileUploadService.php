3aa1daf5e4fa8e4ac8cfd397ce5b856ddbd901ee
timteam <wlekgkxo@gmail.com>
2024-11-27 14:40:54 +0900

    이미지 업로드 및 메타 저장 테스트

--- app/Services/FileUploadService.php
+++ app/Services/FileUploadService.php
@@ -53,7 +53,14 @@ class FileUploadService
 
     public function makeThumbnail($path, $file_name)
     {
-        $thumb_path = env('STORAGE_UPLOAD').'/thumb';
+        // $thumb_file = public_path('storage').'\\upload\\thumb\\'.$file_name;
+        // $command = "exiftool -b -ThumbnailImage ".$path." > ".$thumb_file;
+        // $output = shell_exec($command);
+        // $thumb_j = json_decode($output, true);
+        // $thumb = $thumb_j[0];
+        // return '/storage/upload/thumb/'.$file_name;
+
+        $thumb_path = public_path('storage').'\\upload\\thumb\\';
 
         $thumb = Image::make($path)->resize(200, 200, function ($constraint) {
             $constraint->aspectRatio(); // 원본 비율 유지
@@ -61,9 +68,9 @@ class FileUploadService
         });
 
         // 썸네일 저장
-        $result = $thumb->save($thumb_path.'/'.$file_name);
+        $thumb->save($thumb_path.$file_name);
 
-        return env('STORAGE_ROOT').'/thumb/'.$file_name;
+        return '/storage/upload/thumb/'.$file_name;
     }
 
     public function getExif($path)
